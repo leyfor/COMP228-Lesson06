8b54cd3df5c67c2e957207bd4646d1bd9975265a
leyfor <justman51@yahoo.com>
2018-02-08 11:47:17 +0100

    added custom JDialog class

--- src/ExampleFrame.java
+++ src/ExampleFrame.java
@@ -4,11 +4,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class ExampleFrame extends JFrame {
 	
 	// PRIVATE INSTANCE VARIABLES
 	private JPanel _contentPanel;
+	private EmptyBorder _myEmptyBorder;
+	private BorderLayout _myBorderLayout;
+	private JLabel _firstNameLabel = new JLabel("First Name: ");
+	private String _firstNumber = JOptionPane.showInputDialog("Enter first integer: ");
+	private String _secondNumber = JOptionPane.showInputDialog("Enter second integer: ");
+	private int messageReturn;
+	
 
 	/**
 	 * Launch the application.
@@ -32,12 +41,48 @@ public class ExampleFrame extends JFrame {
 
 	// CONSTRUCTOR +++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	public ExampleFrame() {
+		_Initialize();
+	}
+
+	private void _Initialize() {
+		
+		this._myEmptyBorder = new EmptyBorder(5, 5, 5, 5);
+		this._myBorderLayout = new BorderLayout(0, 0);
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		this._contentPanel = new JPanel();
-		this._contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		this._contentPanel.setLayout(new BorderLayout(0, 0));
+		this._contentPanel.setBorder(this._myEmptyBorder);
+		this._contentPanel.setLayout(this._myBorderLayout);
 		setContentPane(this._contentPanel);
+		
+		//JLabel lblNewLabel = new JLabel("First Name: ");
+		this._contentPanel.add(_firstNameLabel, BorderLayout.NORTH);
+		
+		int number1 = Integer.parseInt(this._firstNumber);
+		int number2 = Integer.parseInt(this._secondNumber);
+		
+		int sum = number1 + number2;
+		
+		JOptionPane.showMessageDialog(null, "The sum is " + sum, "Sum of Two Intergers", JOptionPane.PLAIN_MESSAGE);
+		messageReturn = JOptionPane.showConfirmDialog(null, "Error- Incorrect Input", "Error", JOptionPane.YES_NO_CANCEL_OPTION);
+		String welcome = "You are welcome";
+		String WhatUp = "What do you want to do?";
+		String Bye = "Good Bye";
+		
+	/*	switch(messageReturn) {
+		case 0: 
+			welcome;
+			break;
+		case 1:
+			WhatUp;
+			break;
+		case 2:
+			Bye;
+			break;
+			
+		} */
+		
 	}
 
 }
--- src/Program.java
+++ src/Program.java
@@ -1,12 +1,21 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 //DRIVER CLASSS ++++++++++++++++++++++++++++++
 public class Program {
 
     public static void main(String[] args) {
        
-        ExampleFrame frame = new ExampleFrame();
-		frame.setVisible(true);
-
+       // ExampleFrame  myFrame  = new  ExampleFrame();
+       // myFrame.setVisible(true);
+    	MyDialog myDialog = new MyDialog();
+        
+    	myDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+    	myDialog.setVisible(true);
     }
 
 }
